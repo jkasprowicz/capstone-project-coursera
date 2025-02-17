f57924b4c6491b7ac2a480a38254ce78b1f6652f
jkasprowicz <joao.kasprowicz@univali.br>
2025-02-17 16:28:13 -0300

    updates

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField()
+    booking_data = models.DateTimeField()
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=3, decimal_places=1)
+    inventory = models.IntegerField()
+
+
+
